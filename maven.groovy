7f352caca9be1510d163026de350302005902a86
Eric Reyes Casetti <eric.reyes@solucionativa.cl>
2022-01-31 17:58:58 -0300

    Ajuste maven groovy

--- maven.groovy
+++ maven.groovy
@@ -1,33 +1,57 @@
-pipeline {
-    agent any
-    environment {
-        NEXUS_USER         = credentials('user-nexus')
-        NEXUS_PASSWORD     = credentials('password-nexus')
-    }
-    parameters {
-        choice(
-            name:'compileTool',
-            choices: ['Maven', 'Gradle'],
-            description: 'Seleccione herramienta de compilacion'
-        )
-    }
-    stages {
-        stage("Pipeline"){
-            steps {
-                script{
-                  switch(params.compileTool)
-                    {
-                        case 'Maven':
-                            def ejecucion = load 'maven.groovy'
-                            ejecucion.call()
-                        break;
-                        case 'Gradle':
-                            def ejecucion = load 'gradle.groovy'
-                            ejecucion.call()
-                        break;
-                    }
-                }
-            }
-        }
-    }
+/*
+	forma de invocación de método call:
+	def ejecucion = load 'script.groovy'
+	ejecucion.call()
+*/
+def call(){
+  stage("Paso 1: Compliar"){
+    sh "mvn clean compile -e"
+  }
+  stage("Paso 2: Testear"){
+
+    sh "mvn clean test -e"
+  }
+  stage("Paso 3: Build .Jar"){
+    sh "mvn clean package -e"
+  }
+  stage("Paso 4: Sonar - Análisis Estático"){
+      sh "echo 'Análisis Estático!'"
+      withSonarQubeEnv('sonarqube3') {
+          sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
+      }
+  }
+  stage("Paso 5: Curl Springboot Gradle sleep 20"){
+      sh "gradle bootRun&"
+      sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+  }
+  stage("Paso 6: Subir Nexus"){
+      nexusPublisher nexusInstanceId: 'nexus3',
+      nexusRepositoryId: 'devops-usach-nexus',
+      packages: [
+          [$class: 'MavenPackage',
+              mavenAssetList: [
+                  [classifier: '',
+                  extension: '.jar',
+                  filePath: 'build/DevOpsUsach2020-0.0.1.jar'
+              ]
+          ],
+              mavenCoordinate: [
+                  artifactId: 'DevOpsUsach2020',
+                  groupId: 'com.devopsusach2020',
+                  packaging: 'jar',
+                  version: '0.0.1'
+              ]
+          ]
+      ]
+  }
+  stage("Paso 7: Descargar Nexus"){
+      sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus3:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
+  }
+  stage("Paso 8: Levantar Artefacto Jar"){
+      sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
+  }
+  stage("Paso 9: Testear Artefacto - Dormir(Esperar 20sg) "){
+      sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+  }
 }
+return this;
