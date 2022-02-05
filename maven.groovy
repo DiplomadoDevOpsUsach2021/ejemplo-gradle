7d6330aa375315e5e8495ebd01d40e2adad47d85
Eric Reyes Casetti <eric.reyes@solucionativa.cl>
2022-02-04 23:17:54 -0300

    Se agrega env.Tarea

--- maven.groovy
+++ maven.groovy
@@ -4,28 +4,38 @@
 	ejecucion.call()
 */
 def call(){
-  stage("Paso 1: Compliar"){
+  env.TAREA = 'Paso 1: Compilar'
+  stage("${env.TAREA}"){
     sh "mvn clean compile -e"
   }
-  stage("Paso 2: Testear"){
 
+  env.TAREA = 'Paso 2: Testear'
+  stage("${env.TAREA}"){
     sh "mvn clean test -e"
   }
-  stage("Paso 3: Build .Jar"){
-    sh "mvn clean package -e"
+
+  env.TAREA = 'Paso 3: Build .Jar'
+  stage("${env.TAREA}"){
+      sh "mvn clean package -e"
   }
-  stage("Paso 4: Sonar - Análisis Estático"){
+
+  env.TAREA = 'Paso 4: Sonar - Análisis Estático"'
+  stage("${env.TAREA}"){
       sh "echo 'Análisis Estático!'"
       withSonarQubeEnv('sonarqube') {
           sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
       }
   }
+
+  env.TAREA = 'Paso 5: Curl Springboot Gradle sleep 60'
+  stage("${env.TAREA}"){
   stage("Paso 5: Curl Springboot Gradle sleep 60"){
       sh "gradle bootRun&"
       sh "sleep 60 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
   }
   
-  stage("Paso 6: Subir Nexus"){
+  env.TAREA = 'Paso 6: Subir Nexus'
+  stage("${env.TAREA}"){
       nexusPublisher nexusInstanceId: 'nexus',
       nexusRepositoryId: 'devops-usach-nexus',
       packages: [
@@ -45,13 +55,19 @@ def call(){
           ]
       ]
   }
-  stage("Paso 7: Descargar Nexus"){
+
+  env.TAREA = 'Paso 7: Descargar Nexus'
+  stage("${env.TAREA}"){
       sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
   }
-  stage("Paso 8: Levantar Artefacto Jar"){
+
+  env.TAREA = 'Paso 8: Levantar Artefacto Jar'
+  stage("${env.TAREA}"){
       sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
   }
-  stage("Paso 9: Testear Artefacto - Dormir(Esperar 20sg) "){
+
+  env.TAREA = 'Paso 9: Testear Artefacto - Dormir(Esperar 20sg) '
+  stage("${env.TAREA}"){
       sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
   }
   
